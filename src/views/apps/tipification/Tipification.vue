e2b62680874368a9f3202ed8a3c8d461f5f9f84d
nicoblasco <nicoblasco@hotmail.com>
2020-07-11 17:15:46 -0300

    first commit

---
+++ src/views/apps/tipification/Tipification.vue
@@ -0,0 +1,15 @@
+<template>
+	<router-view :key="$route.fullPath"></router-view>
+</template>
+
+<script>
+
+
+export default {
+	name: 'Tipification'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  lintOnSave: false,
+  //baseUrl: '/dist/',
+  productionSourceMap: false,
+  configureWebpack: {
+    //entry: ["babel-polyfill", "./src/main.js"]
+  }
+}
